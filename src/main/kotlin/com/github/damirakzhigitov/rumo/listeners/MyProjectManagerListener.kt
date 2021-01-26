661e133f930700b95be214cc7ba926f295f47d9e
GitHub Action <action@github.com>
2021-01-26 22:09:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/damirakzhigitov/rumo/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/damirakzhigitov/rumo/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.damirakzhigitov.rumo.listeners
 
+import com.github.damirakzhigitov.rumo.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/damirakzhigitov/rumo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/damirakzhigitov/rumo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.damirakzhigitov.rumo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.damirakzhigitov.rumo.MyBundle
 
 class MyApplicationService {
 
